74f67f9b3bb8de054f8f904d6264943984e2ed89
hhadland <hadlandh@oregonstate.edu>
2022-04-05 12:57:00 -0500

    Final product for submission.

--- main.py
+++ main.py
@@ -1,21 +1,68 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import datetime
 
 from flask import Flask, render_template
 
+# [START gae_python38_datastore_store_and_fetch_times]
+# [START gae_python3_datastore_store_and_fetch_times]
+from google.cloud import datastore
+
+datastore_client = datastore.Client()
+
+# [END gae_python3_datastore_store_and_fetch_times]
+# [END gae_python38_datastore_store_and_fetch_times]
 app = Flask(__name__)
 
 
+# [START gae_python38_datastore_store_and_fetch_times]
+# [START gae_python3_datastore_store_and_fetch_times]
+def store_time(dt):
+    entity = datastore.Entity(key=datastore_client.key('visit'))
+    entity.update({
+        'timestamp': dt
+    })
+
+    datastore_client.put(entity)
+
+
+def fetch_times(limit):
+    query = datastore_client.query(kind='visit')
+    query.order = ['-timestamp']
+
+    times = query.fetch(limit=limit)
+
+    return times
+# [END gae_python3_datastore_store_and_fetch_times]
+# [END gae_python38_datastore_store_and_fetch_times]
+
+
+# [START gae_python38_datastore_render_times]
+# [START gae_python3_datastore_render_times]
 @app.route('/')
 def root():
-    # For the sake of example, use static information to inflate the template.
-    # This will be replaced with real information in later steps.
-    dummy_times = [datetime.datetime(2018, 1, 1, 10, 0, 0),
-                   datetime.datetime(2018, 1, 2, 10, 30, 0),
-                   datetime.datetime(2018, 1, 3, 11, 0, 0),
-                   ]
+    # Store the current access time in Datastore.
+    store_time(datetime.datetime.now(tz=datetime.timezone.utc))
 
-    return render_template('index.html', times=dummy_times)
+    # Fetch the most recent 10 access times from Datastore.
+    times = fetch_times(10)
 
+    return render_template(
+        'index.html', times=times)
+# [END gae_python3_datastore_render_times]
+# [END gae_python38_datastore_render_times]
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
